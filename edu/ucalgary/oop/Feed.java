d912346832daae3f78e8ecd78ebcbc89b29ce611
ebubea1 <95447432+ebubea1@users.noreply.github.com>
2024-03-28 10:14:01 -0600

    commented printTreatments() in Schedule

--- edu/ucalgary/oop/Feed.java
+++ edu/ucalgary/oop/Feed.java
@@ -1,5 +1,7 @@
 package edu.ucalgary.oop;
-
+// TODO - Making a checklist here for now
 // todo: make a UML diagram for this class before implmentation
+// TODO - information must be written to a file
+// TODO - Unit Testing
 public class Feed {
 }
--- edu/ucalgary/oop/MainExecution.java
+++ edu/ucalgary/oop/MainExecution.java
@@ -10,6 +10,7 @@ import java.util.*;
 public class MainExecution {
     
     public static void main(String[] args){
+
         
     }
 }
