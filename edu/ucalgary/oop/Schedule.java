06217d99f84e68602956373030e80f19e2758478
ebubea1 <95447432+ebubea1@users.noreply.github.com>
2024-03-28 11:21:01 -0600

    Completed commenting of Schedule

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -30,31 +30,34 @@ public class Schedule{
          * Prints the treatments of the given animal for each hour
          */
 
-        int time;                                   // Time variable to store the StartHour
-        String name, description = "";              // 
-        int lastTime = -1;                          // What is this negative for?
+        int time;                                               // Time variable to store the StartHour
+        String name, description = "";                          //
+        int lastTime = -1;                                      // What is this negative for?
         //Collections.sort(this.treatments);//sorts by time
         for (Treatment t : this.treatments){
             // For loop that goes through the treatment array
             // For each treatment, accesses information form the given index
-            time = t.getStartHour();                // Initialize time with the StartHour from the Treatment index
-            if(lastTime!=time){System.out.println(String.format("\n%02d:00",time));}
-            lastTime=time;
-            name = t.getAnimalName();
-            description = t.getDescription();
-            this.dayHours[time] -= t.getDuration();
-            System.out.print("* " + description + " (" + name +") - ");
-            System.out.println("Time remaining in hour: " + this.dayHours[time]);
+            time = t.getStartHour();                                    // Initialize time with the StartHour from the Treatment index
+            if(lastTime != time){                                       // Checks if lastTime is equal to
+                System.out.println(String.format("\n%02d:00",time));    // Prints out the time of the treatment
+            }
+            lastTime = time;                                            // Equates the treatment time to lastTime
+                                                                        // Done to ensure that the time is not printed again
+            name = t.getAnimalName();                                   // Assign the animalName to the name variable
+            description = t.getDescription();                           // Assign the description to the description variable
+            this.dayHours[time] -= t.getDuration();                     // Subtract the duration of the task form the hour of the day
+            System.out.print("* " + description + " (" + name +") - "); // Prints the description of the task and the name of the animal
+            System.out.println("Time remaining in hour: " + this.dayHours[time]); // Pirnts the remaining time in the given hour for tasks
         }
     }
 
     public static void main(String[] args){
-        String url = "jdbc:postgresql://localhost:5432/ewr";
-        String username = "oop";
-        String password = "ucalgary";
-        SQLData myJDBC = new SQLData(url,username,password);
+        String url = "jdbc:postgresql://localhost:5432/ewr";            // Database url
+        String username = "oop";                                        // Username for the database ewr
+        String password = "ucalgary";                                   // Password for the database ewr
+        SQLData myJDBC = new SQLData(url,username,password);            // Instantiates an SQLData object with the url, username and password
         
-        Schedule schedule = new Schedule(myJDBC.getAnimalList(), myJDBC.getTreatmentList());        
+        Schedule schedule = new Schedule(myJDBC.getAnimalList(), myJDBC.getTreatmentList());  // Instantiates a Schedule with information form the AnimalList
     }
 
 }
