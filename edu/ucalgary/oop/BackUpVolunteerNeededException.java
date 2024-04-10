b55e725b99737f0638887d4015228ee88515add2
Daniel Lee <150202632+danieljsklee@users.noreply.github.com>
2024-04-10 04:51:01 -0600

    Update BackUpVolunteerNeededException.java
    Added additional documentation about the function.

--- edu/ucalgary/oop/BackUpVolunteerNeededException.java
+++ edu/ucalgary/oop/BackUpVolunteerNeededException.java
@@ -1,19 +1,41 @@
 package edu.ucalgary.oop;
 
+/**
+ * The BackUpVolunteerNeededException class represents an exception that is thrown
+ * when a backup volunteer is needed for a specific task.
+ * It extends the Exception class.
+ */
 public class BackUpVolunteerNeededException extends Exception {
 
+    // The DailyTasks object associated with the exception
     private DailyTasks task;
 
-    BackUpVolunteerNeededException(String error, DailyTasks task){
+    /**
+     * Constructs a BackUpVolunteerNeededException with the specified error message
+     * and the associated DailyTasks object.
+     *
+     * @param error the error message
+     * @param task  the DailyTasks object associated with the exception
+     */
+    BackUpVolunteerNeededException(String error, DailyTasks task) {
         super(error);
         this.task = task;
     }
-    BackUpVolunteerNeededException(String error){
+
+    /**
+     * Constructs a BackUpVolunteerNeededException with the specified error message.
+     *
+     * @param error the error message
+     */
+    BackUpVolunteerNeededException(String error) {
         super(error);
     }
-   
 
-
+    /**
+     * Returns the DailyTasks object associated with the exception.
+     *
+     * @return the DailyTasks object
+     */
     public DailyTasks getTask() {
         return task;
     }
